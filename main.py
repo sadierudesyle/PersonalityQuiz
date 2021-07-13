886d5504789a5ba3b83704a04d91bd1a4123a18f
sadierudesyle <>
2021-07-13 14:57:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,352 @@
+# Display a message 
+#Create a list and append the answers to the list to help decide the outcome
+def personality_quiz():
+
+    owl_total = 0
+    giraffe_total = 0
+    dragon_total = 0
+    unicorn_total = 0
+
+    print()
+    print("✰✰✰ Hello! ✰✰✰")
+    print("Answer the following questions to find out which animal you represent most!")
+    print()
+    print("Question 1, what is your favorite social media app? 📱  Choose A, B, C, D")
+    print("A.) Twitter")
+    print("B.) Facebook")
+    print("C.) Instagram")
+    print("D.) Snapchat")
+    print()
+
+    while True:
+        question_1 = input("> ")
+
+        if question_1.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_1.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_1.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_1.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 2, which is your favorite color out of the four? 🎨  Choose A, B, C, D")
+    print("A.) Black")
+    print("B.) Orange")
+    print("C.) Green")
+    print("D.) Pink")
+    print()
+
+    while True:
+        question_2 = input("> ")
+
+        if question_2.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_2.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_2.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_2.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 3, which best describes how you react in an argument? (ง'̀-'́)ง Choose A, B, C, D")
+    print("A.) Calm, cool and collected")
+    print("B.) Observant before acting")
+    print("C.) React with your feelings")
+    print("D.) Try to defuse the argument")
+    print()
+
+    while True:
+        question_3 = input("> ")
+
+        if question_3.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_3.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_3.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_3.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 4, how do you like your coffee? ☕  Choose A, B, C, D")
+    print("A.) Black")
+    print("B.) Some cream, little sugar")
+    print("C.) As many shots of espresso that will fit")
+    print("D.) Sugar, cream, whipped cream, flavoring, sprinkles, everything!")
+    print()
+
+    while True:
+        question_4 = input("> ")
+
+        if question_4.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_4.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_4.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_4.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 5, what is your favorite genre of movies? 🎥  Choose A, B, C, D")
+    print("A.) Horror/Mystery")
+    print("B.) Comedy")
+    print("C.) Fantasy")
+    print("D.) Romance")
+    print()
+
+    while True:
+        question_5 = input("> ")
+
+        if question_5.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_5.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_5.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_5.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 6, what's your perfect Saturday night? 🥳  Choose A, B, C, D")
+    print("A.) All by myself, watching movies and ordering pizza")
+    print("B.) Going out to eat with friends")
+    print("C.) Getting dressed up and going to a party")
+    print("D.) Having a game night with friends")
+    print()
+
+    while True:
+        question_6 = input("> ")
+
+        if question_6.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_6.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_6.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_6.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 7, if you were to win one award, what would it be?  🏆  Choose A, B, C, D")
+    print("A.) Heaviest sleeper")
+    print("B.) Tallest friend")
+    print("C.) Biggest hothead")
+    print("D.) Brightest smile")
+    print()
+
+    while True:
+        question_7 = input("> ")
+
+        if question_7.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_7.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_7.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_7.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+    
+    print()
+    print("Question 8, what snack would you choose over the others?  🍿  Choose A, B, C, D")
+    print("A.) Granola bar")
+    print("B.) Apple")
+    print("C.) Takis")
+    print("D.) Starbursts")
+    print()
+
+    while True:
+        question_8 = input("> ")
+
+        if question_8.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_8.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_8.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_8.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 9, which series could you watch all the way through?  📺  Choose A, B, C, D")
+    print("A.) Stranger Things")
+    print("B.) Tiger King")
+    print("C.) The Office")
+    print("D.) Gossip Girl")
+    print()
+
+    while True:
+        question_9 = input("> ")
+
+        if question_9.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_9.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_9.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_9.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    print()
+    print("Question 10, if you're stressed, what do you do to relax your mind?  💤  Choose A, B, C, D")
+    print("A.) Take a nap")
+    print("B.) Exercise")
+    print("C.) Crack a cold one")
+    print("D.) Hangout with your friends")
+    print()
+
+    while True:
+        question_10 = input("> ")
+
+        if question_10.upper() == 'A':
+            owl_total += 1
+            break
+
+        if question_10.upper() == 'B':
+            giraffe_total += 1
+            break
+
+        if question_10.upper() == 'C':
+            dragon_total += 1
+            break
+
+        if question_10.upper() == 'D':
+            unicorn_total += 1
+            break
+
+        else: print("Please choose A, B, C or D")
+
+    import time
+
+    timer = 25
+
+    print()
+    print("Calculating results...")
+
+    while timer <= 100:
+        print(timer, "%")
+        time.sleep(1)
+        timer = timer + 25
+
+    if owl_total > giraffe_total and owl_total > dragon_total and owl_total > unicorn_total:
+        print()
+        print("Your personality best fits an owl! You are detail oriented and hate waking up early, you can easily adapt to any situation you're put into, and you have extensive patience. You're an introvert but when you warm up to someone you show your true, outgoing self! Once someone gets to know you they can't stay away from you! 🦉")
+
+    elif giraffe_total > owl_total and giraffe_total > dragon_total and giraffe_total > unicorn_total:
+        print()
+        print("Your personality best fits a giraffe! You are very observant as an individual, and look into others feelings before you take action. You are seen as the friend to go to for advice and comfort. When someone is looking for guidance they turn to you. When in a situation, you look at the big picture rather than the small factors. 🦒")
+
+    elif dragon_total > owl_total and dragon_total > giraffe_total and dragon_total > unicorn_total:
+        print()
+        print("Your personality best fits a dragon! As a dragon, you are ambitious and strive to achieve your goals. You love to be adventurous and travel to places you've never been to. You often never feel tired and are always full of light and energy. There is a sense of charm to your personality that makes you very likeable 🐲")
+
+    elif unicorn_total > owl_total and unicorn_total > giraffe_total and unicorn_total > dragon_total:
+        print()
+        print("Your personality best fits a unicorn! Your radiant personality shows to everyone you meet. You are very unique but everyone you come across strives to have your mentality. Your downfall can be seen as self-centered, but that's only because you recognize how amazing you are! Remember not to change yourself to make others happy. 🦄")
+
+    else: 
+        print()
+        print("Woah! You're a combination of multiple animals... That means you represent a Pegasus most! You are a gentle being, appreciating every feature of eveything you encounter. You have a freedom within yourself that is difficult to obtain. You live your life to the fullest and appreciate true beauty. You are quirky and you never let your guard down. You appreciate every day that you get to live and don't take anything for granted. ƸӜƷ")
+
+personality_quiz()
+
+while True:
+    print()
+    print("Would you like to take the quiz again? Y or N?")
+    take_again = input("> ")
+
+    if take_again.upper() == "Y":
+        personality_quiz()
+
+    elif take_again.upper() == "N":
+        print()
+        print("Thank you for taking my quiz! ♡♡♡")
+        break
+
+    else: 
+        print()
+        print("Please type Y or N :) ")
